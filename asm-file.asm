e69ec97508010d40490fb4f50dbe92af004718c0
MahaAther <mahaather975@gmail.com>
2024-12-30 12:33:39 +0500

    asm.task

--- asm-file.asm
+++ asm-file.asm
@@ -1,28 +1,31 @@
-INCLUDE Irvine32.inc
+INCLUDE Irvine32.inc 
 .data
-msg db "total positive number",0     ;msg to display
-array sword -3,-7,-4,-5,1,2,3,7,9    ;array
-count dword ?                        ;variable for store total positive num
-.code
-asmfunc PROC 
-   mov esi,offset array              ;move first index of array to esi
-   mov ecx,lengthof array            ;ecx=array lenght means loops ends when array lenght ends
-   mov eax,0                         ;declare eax,0 because we display count with eax 
-checkloop:                           ;loop to check if num is positive or not
-   movsx ebx,word ptr [esi]          ;sign-extend esi into ebx
-   test ebx,8000h                    ;test ebx(array elments) with 8000 to check M.S.B
-   jnz skip                          ;jump to label if zero flag not set (means num is negative)
-   inc count                         ;if positive then do inc in count variable 
-skip:                                  
-   add esi,type word                 ;incremnet esi with type of word to move on next element
-   loop checkloop                    ;looping until (ecx<0)
-;display total positive number in an array
-mov edx,offset msg                   ;msg for total even num
-call writestring
-call crlf
-mov eax,count                        ;mov count to eax to display
-call writeint                        ;display eax(total positive num)
-call crlf                           
-ret                                  ;program ends
-asmfunc ENDP                         ;end of asmfunc proc
-end                                   
+msg2 byte "total positive num",0
+.code 
+asmfunc PROC array:DWORD, p2:DWORD
+    mov eax, 0           ; Clear eax (not used directly here)
+    mov ebx, 0           ; Counter for positive numbers
+    mov esi, array       ; Load address of array into esi
+    mov ecx, p2          ; Load the count of array elements into ecx
+    
+next:
+    mov eax, [esi]       ; Load current element into eax
+    cmp eax, 0           ; Compare with zero
+    jle skip             ; If less than or equal to zero, skip
+    inc ebx              ; Increment counter for positive numbers
+
+skip:
+    add esi, TYPE array  ; Move to the next element
+    loop next            ; Repeat for all elements
+    
+    ; Print total positive numbers
+    mov edx, offset msg2 ; Load the address of msg2 into edx
+    call writestring     ; Print the message string
+    call crlf            ; Newline
+    mov eax, ebx         ; Move the count of positive numbers into eax
+    call writeint        ; Print the positive count
+    call crlf            ; Newline
+    
+    ret
+asmfunc ENDP
+end
