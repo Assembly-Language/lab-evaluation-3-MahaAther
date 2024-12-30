8057f35c6a8b553115fe52e718e259817527c4cc
MahaAther <mahaather975@gmail.com>
2024-12-30 12:46:37 +0500

    asm-file

--- asm-file.asm
+++ asm-file.asm
@@ -7,17 +7,14 @@ asmfunc PROC array:DWORD, p2:DWORD
     mov ebx, 0           ; Counter for positive numbers
     mov esi, array       ; Load address of array into esi
     mov ecx, p2          ; Load the count of array elements into ecx
-    
 next:
     mov eax, [esi]       ; Load current element into eax
     cmp eax, 0           ; Compare with zero
     je skip             ; If less than or equal to zero, skip
     inc ebx              ; Increment counter for positive numbers
-
 skip:
     add esi, TYPE array  ; Move to the next element
     loop next            ; Repeat for all elements
-    
     ; Print total positive numbers
     mov edx, offset msg2 ; Load the address of msg2 into edx
     call writestring     ; Print the message string
