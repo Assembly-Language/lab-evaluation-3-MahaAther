42ec14d532f9b4e1a19cf57a3d87492457284c81
MahaAther <mahaather975@gmail.com>
2024-12-30 11:34:58 +0500

    asm.task

--- asm-file.asm
+++ asm-file.asm
@@ -1,27 +1,28 @@
-
-
 INCLUDE Irvine32.inc
 .data
 ;public asmfunc
-msg db "assebly procedure end now",0
-
-
+msg db "total positive number",0
+array sword -3,-7,-4,-5,1,2,3,7,9
+count dword ?
 .code
-asmfunc PROC p1:DWORD, p2:DWORD
-    
-    push ebp
-    mov ebp,esp
-    mov eax, [ebp+12
-\] ; Move the first parameter into EAX
-    call writedec
-    call crlf
-    ;add eax, p2 ; Add the second parameter to EAX
-    call writedec
-    call crlf
-    mov edx, offset msg
-    call writestring
-    call crlf
-    
-    ret
+asmfunc PROC
+   mov esi,offset array
+   mov ecx,lengthof array
+   mov eax,0       ;declare eax,0 because we display count with eax 
+checkloop: 
+   movsx ebx,word ptr [esi]
+   test ebx,8000h
+   jnz skip
+   inc count
+skip: 
+   add esi,type word
+   loop checkloop
+;display total positive number in an array
+mov edx,offset msg
+call writestring
+call crlf
+mov eax,count
+call writeint
+call crlf
 asmfunc ENDP
 end
