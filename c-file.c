48a543e62b966b29657515b627ddaaf58b1fdf82
MahaAther <mahaather975@gmail.com>
2024-12-30 12:55:31 +0500

    c file

--- c-file.c
+++ c-file.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
+
+
 //extern "C" void __stdcall asmfunc(void);
+
 #ifdef __cplusplus
 extern "C" {
 #endif
-void __stdcall asmfunc(int array[],int length);
+
+void __stdcall asmfunc(int array[],int length );
+
 #ifdef __cplusplus
 }
 #endif
@@ -15,8 +20,13 @@ int main() {
     printf("assembly proc calling from  from C! \n");
 getch();
     int array[8] = {-1,-2,-3,1,2,3,4,5};
+    
+    
      asmfunc(array,8); //assembly proc calling
+   
    getch();
+    
+    
     printf("back to  C! \n"); // printing in c
     
   
