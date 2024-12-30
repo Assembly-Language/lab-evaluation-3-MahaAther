246516cdae58a5c6d6b4ad65dac4a1ffc560a1b6
MahaAther <mahaather975@gmail.com>
2024-12-30 11:49:08 +0500

    asm.task

--- asm-file.asm
+++ asm-file.asm
@@ -1,28 +1,28 @@
 INCLUDE Irvine32.inc
 .data
-;public asmfunc
-msg db "total positive number",0
-array sword -3,-7,-4,-5,1,2,3,7,9
-count dword ?
+msg db "total positive number",0     ;msg to display
+array sword -3,-7,-4,-5,1,2,3,7,9    ;array
+count dword ?                        ;variable for store total positive num
 .code
-asmfunc PROC
-   mov esi,offset array
-   mov ecx,lengthof array
-   mov eax,0       ;declare eax,0 because we display count with eax 
-checkloop: 
-   movsx ebx,word ptr [esi]
-   test ebx,8000h
-   jnz skip
-   inc count
-skip: 
-   add esi,type word
-   loop checkloop
+asmfunc PROC 
+   mov esi,offset array              ;move first index of array to esi
+   mov ecx,lengthof array            ;ecx=array lenght means loops ends when array lenght ends
+   mov eax,0                         ;declare eax,0 because we display count with eax 
+checkloop:                           ;loop to check if num is positive or not
+   movsx ebx,word ptr [esi]          ;sign-extend esi into ebx
+   test ebx,8000h                    ;test ebx(array elments) with 8000 to check M.S.B
+   jnz skip                          ;jump to label if zero flag not set (means num is negative)
+   inc count                         ;if positive then do inc in count variable 
+skip:                                  
+   add esi,type word                 ;incremnet esi with type of word to move on next element
+   loop checkloop                    ;looping until (ecx<0)
 ;display total positive number in an array
-mov edx,offset msg
+mov edx,offset msg                   ;msg for total even num
 call writestring
 call crlf
-mov eax,count
-call writeint
-call crlf
-asmfunc ENDP
-end
+mov eax,count                        ;mov count to eax to display
+call writeint                        ;display eax(total positive num)
+call crlf                           
+ret                                  ;program ends
+asmfunc ENDP                         ;end of asmfunc proc
+end                                   
